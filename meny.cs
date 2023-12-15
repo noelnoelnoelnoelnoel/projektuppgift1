294730c97346c05f998917584535fbe5387af9ee
noelnoelnoelnoelnoel <144672920+noelnoelnoelnoelnoel@users.noreply.github.com>
2023-12-15 17:14:40 +0100

    Update meny.cs

--- meny.cs
+++ meny.cs
@@ -9,11 +9,15 @@ namespace ProjektkUppgiftGrupp29
             
         bool doAgain = true;
         bool start = true;
+            
             while(start)
             {
 
-            
+            double num1 = Convert.ToInt32(Console.ReadLine());
 
+            double num2 = Convert.ToInt32(Console.ReadLine());
+        
+            char Operator = Convert.ToChar(Console.ReadLine);
 
 
                 while(doAgain)
