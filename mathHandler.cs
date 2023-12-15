05117702b30f79a90f5593262960dd8930701461
noelnoelnoelnoelnoel <144672920+noelnoelnoelnoelnoel@users.noreply.github.com>
2023-12-15 17:30:37 +0100

    Update mathHandler.cs

--- mathHandler.cs
+++ mathHandler.cs
@@ -4,7 +4,50 @@ namespace ProjektkUppgiftGrupp29
 	public class mathHandler
 	{
 
-   
+
+	 public static double (double a, double b, double c, char d)  
+	 {
+		
+		 switch(c)
+		 {
+			case	'+':
+
+				a + b = c
+				 break;
+				 
+			case	'-':
+				 
+				a - b = c
+				 break;
+				 
+			case	'*':
+				 
+				 a * b = c
+				 break;
+				 
+			case	'/':
+				 
+				 a / b = c
+				 break;
+				 
+			case	'^':
+				 
+				 for (int i = 1; i< b.length; i++)
+				 {
+
+				a * a = c; 
+				 }
+					 
+				 break;
+		
+		 }
+
+		 return c;
+		 
+	 }
+		
     	}
+
+	
 }
 
