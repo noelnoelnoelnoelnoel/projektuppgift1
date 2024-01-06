59c0c8c92a08486b677bf9f00f14bd40fa707cc1
noelnoelnoelnoelnoel <144672920+noelnoelnoelnoelnoel@users.noreply.github.com>
2024-01-06 16:00:08 +0100

    Update meny.cs

--- meny.cs
+++ meny.cs
@@ -1,53 +1,81 @@
-mespace GIK299_Grupp_29__Projekt
+using System;
+namespace GIK299_Grupp_29__Projekt
 {
-    public class meny
+    public class Meny
     {
         public static void getMeny()
         {
             bool doAgain = false;
             bool start = true;
-            int counterr = 0;
-            int counter;
-
+            int counterr = 0; // räknar plats i listan
+            int counter;   // räknar plats av utskrift i listan
+            bool checkEx = true;
 
 
             List<mathHandler> tal = new List<mathHandler>();
 
-            while (start)
+            while (checkEx)
             {
-
-                tal.Add(mathHandler.calculate());
                 
-                Console.WriteLine(tal[counterr].ToString());
-
-                counterr++;
-
-                counter = counterr-1;
-
-                Console.WriteLine("Vill du fortsätta på beräkningen  Y/N");
-
-                string answer = Console.ReadLine().ToUpper();
-
-                if(answer == "Y") {doAgain = true; } else { doAgain = false; }
-
-                while (doAgain)
+                while (start)
                 {
 
 
 
-                        tal.Add(mathHandler.continoue(tal[counter]));
+                    try
+                    {
 
-                        Console.WriteLine(tal[counter+1].ToString());
+                        tal.Add(mathHandler.calculate());
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine("Fel format, skriv om tack");
+                        break;
+                    }
+
+                    Console.WriteLine(tal[counterr].ToString());
+
+                    counterr++;
+
+                    counter = counterr - 1;  // påbörja uträkning från senaste utskrift i (A)
+
+                    Console.WriteLine("Vill du fortsätta på beräkningen  Y/N Q för att avsluta");
+
+                    string answer = Console.ReadLine().ToUpper();
+
+                    if (answer == "Y") { doAgain = true; } else if (answer == "Q") { start = false; checkEx = false; } else { doAgain = false; }
+
+                    while (doAgain)
+                    {
+
+
+
+                        try
+                        {
+
+                            tal.Add(mathHandler.continoue(tal[counter]));
+                        }
+                        catch (FormatException ex)
+                        {
+                            Console.WriteLine("Fel format");
+                            Console.WriteLine("Programmet börjar om...");
+                            break;
+                        }
+
+                        Console.WriteLine(tal[counter + 1].ToString()); // (A)
                         counter++;
 
-                    Console.WriteLine("Fortsätta Y/N");
-                    string answerr = Console.ReadLine().ToUpper();
+                        Console.WriteLine("Fortsätta Y/N Q för avsluta");
+                        string answerr = Console.ReadLine().ToUpper();
 
-                    if (answerr == "N") { doAgain = false; } else { doAgain = true; }
+                        if (answerr == "N") { doAgain = false; } else if (answerr == "Q") { start = false; checkEx = false; doAgain = false; } else { doAgain = true; }
+                    }
+
                 }
 
-
             }
+
+            Console.WriteLine("Programmet avslutas...");
         }
     }
 }
